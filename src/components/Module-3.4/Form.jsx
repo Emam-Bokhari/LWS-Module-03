e67f3b18d83fa01b9d18628063f87f49f9588b1e
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2024-03-22 12:40:42 +0600

    principle group of state 2nd example

--- src/components/Module-3.4/Form.jsx
+++ src/components/Module-3.4/Form.jsx
@@ -1,18 +1,37 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 
 export default function Form() {
+
+    const [inputs, setInputs] = useState([{
+      id: 1,
+        label: "input"
+    }])
+    console.log(inputs)
+    function handleAddInput() {
+
+        const nextId = inputs[inputs.length - 1].id + 1
+        setInputs([
+            ...inputs,
+            {
+                id: nextId,
+                label: "input"
+            }
+        ])
+    }
+
     return (
         <Fragment>
-            <div>
-                <input className="" type="text" name="" id="" />
-            </div>
 
-            <div style={
-                {
-                    marginTop: "20px"
-                }
-            } >
-                <button>
+            {inputs.map((input) => (
+                <div className="mb-3" key={input.id} >
+                    <input className="border-2 border-gray-500" type="text" name="" id="" />
+                </div>
+            ))}
+
+            <div className="mt-3">
+                <button
+                    onClick={handleAddInput}
+                    className="bg-indigo-500 px-3 py-1 rounded-md text-white hover:bg-indigo-600" >
                     Add Input
                 </button>
             </div>
